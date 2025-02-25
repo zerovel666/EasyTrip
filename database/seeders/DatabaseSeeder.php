caf0402ae4c401362be41b1bc40522c17a5484ef
zerovel666 <zerovel060606@gmail.com>
2025-02-25 12:22:10 +0500

    correct databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
 
         // Country::factory(50)->create();
         // User::factory(50)->create();
-        LikeCountry::factory(300)->create();
-        DescriptionCountry::factory(300)->create();
+        LikeCountry::factory(rand(100,500))->create();
+        DescriptionCountry::factory(rand(100,500))->create();
         Country::all()->each(function ($country) {
             Tags::factory()->count(rand(3, 4))->create([
                 'country_id' => $country->id
