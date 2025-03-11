838b64a081eb177dd4afe3982ffb9a9a5f1c2ebd
zerovel666 <zerovel060606@gmail.com>
2025-03-11 23:54:45 +0500

    add try/catch

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -15,56 +15,73 @@ class BookingController extends Controller
 {
     public function createBooking(Request $request)
     {
-        return DB::transaction(function () use ($request) {
-            $country = Country::where('trip_name', $request->trip_name)->firstOrFail();
-
-            if ($country->occupied + $request->occupied_place > $country->count_place) {
-                throw new \Exception("Недостаточно свободных мест", 400);
-            }
-
-            $overlappingBookings = Booking::where('country_id', $country->id)
-                ->where('active', true)
-                ->where(function ($query) use ($request) {
-                    $query->whereBetween('check_in', [$request->check_in, $request->check_out])
-                        ->orWhereBetween('check_out', [$request->check_in, $request->check_out])
-                        ->orWhere(function ($q) use ($request) {
-                            $q->where('check_in', '<', $request->check_in)
-                                ->where('check_out', '>', $request->check_out);
-                        });
-                })
-                ->exists();
-
-            if ($overlappingBookings) {
-                throw new \Exception("Выбранные даты уже забронированы", 400);
-            }
-
-            Booking::create([
-                'country_id'     => $country->id,
-                'user_id'        => $request->header('userid'),
-                'check_in'       => $request->check_in,
-                'check_out'      => $request->check_out,
-                'active'         => true,
-                'uuid'           => Str::uuid(),
-                'users_iins'          => json_encode($request->users_iins),
-                'occupied_place' => $request->occupied_place
+        try {
+            $validate = $request->validate([
+                'trip_name' => 'required',
+                'check_in' => 'required|date',
+                'check_out' => 'required|date',
+                'occupied_place' => 'required|integer',
+                'users_iins' => 'required|array',
             ]);
+            if (!$validate) {
+                throw new \Exception("Validation error", 400);
+            }
+            DB::transaction(function () use ($request) {
+                $country = Country::where('trip_name', $request->trip_name)->firstOrFail();
 
+                if ($country->occupied + $request->occupied_place > $country->count_place) {
+                    throw new \Exception("Недостаточно свободных мест", 400);
+                }
+
+                $overlappingBookings = Booking::where('country_id', $country->id)
+                    ->where('active', true)
+                    ->where(function ($query) use ($request) {
+                        $query->whereBetween('check_in', [$request->check_in, $request->check_out])
+                            ->orWhereBetween('check_out', [$request->check_in, $request->check_out])
+                            ->orWhere(function ($q) use ($request) {
+                                $q->where('check_in', '<', $request->check_in)
+                                    ->where('check_out', '>', $request->check_out);
+                            });
+                    })
+                    ->exists();
+
+                if ($overlappingBookings) {
+                    throw new \Exception("Выбранные даты уже забронированы", 400);
+                }
+
+                Booking::create([
+                    'country_id'     => $country->id,
+                    'user_id'        => $request->header('userid'),
+                    'check_in'       => $request->check_in,
+                    'check_out'      => $request->check_out,
+                    'active'         => true,
+                    'uuid'           => Str::uuid(),
+                    'users_iins'          => json_encode($request->users_iins),
+                    'occupied_place' => $request->occupied_place
+                ]);
+
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Бронь создана пожалуйста оплатите для ее активации',
+                ], 200);
+            });
+        } catch (\Exception $e) {
             return response()->json([
-                'status' => true,
-                'message' => 'Бронь создана пожалуйста оплатите для ее активации',
-            ], 200);
-        });
+                'status' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        }
     }
 
     public function allByTripName(Request $request)
     {
-        if ($request->header('userid') != null){
+        if ($request->header('userid') != null) {
             $user = User::where('id', $request->header('userid'))->first();
-        }        
-        if (isset($user) && $user->role == 'admin'){
+        }
+        if (isset($user) && $user->role == 'admin') {
             $country = Country::whereTripName($request->trip_name)->get()[0];
             return Booking::whereCountryId($country->id)->get();
-        }else{
+        } else {
             $country = Country::whereTripName($request->trip_name)->get()[0];
             return Booking::whereCountryId($country->id)->whereActive(true)->get();
         }
@@ -81,15 +98,33 @@ class BookingController extends Controller
     }
     public function updateById(Request $request, $id)
     {
-        $data = $request->all();
-        return Booking::where('id', $id)->update($data);
+        try {
+            $data = $request->all();
+            Booking::where('id', $id)->update($data);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success update'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        }
     }
     public function deleteById($id)
     {
-        return response()->json([
-            'status' => Booking::where('id', $id)->delete(),
-            'message' => "Success delete"
-        ],200);
+        try {
+            return response()->json([
+                'status' => Booking::where('id', $id)->delete(),
+                'message' => "Success delete"
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        }
     }
     public function downloadTableColumnOrThisRelation()
     {
